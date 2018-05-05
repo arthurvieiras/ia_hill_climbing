8886e2288a8a488b0d57873fe1864dc7675c96ca
Lucas Barros <lbpo91@gmail.com>
2018-05-05 19:17:50 -0300

    incluindo algoritmo de busca e ajuste no EGO-readme

---
+++ busca.py
@@ -0,0 +1,28 @@
+import functions
+
+
+def dfs_recursive(graph, vertex, path=[], limit=4):
+    path += [vertex]
+    # print('visiting', vertex)
+    limit -= 1
+    # print('Limit is on', limit)
+
+    if limit > 0:
+        if vertex in graph.keys():
+            for neighbor in graph[vertex]:
+                if neighbor not in path:
+                    # print(vertex, 'is Parent')
+                    path = dfs_recursive(graph, neighbor, path, limit)
+    #         print('End for', vertex)
+    #     else:
+    #         print(vertex, 'was Leaf')
+    # else:
+    #     print('Limit reached')
+
+    return path
+
+
+def assert_influence(node_key, influence_value):
+    graph = functions.carrega_edges()
+    value = dfs_recursive(graph, node_key).__len__()
+    return value == influence_value
