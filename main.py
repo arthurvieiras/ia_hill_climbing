663f67f2051ba711ae24e835b0a1cdaea8fc0f1b
Lucas Barros <lbpo91@gmail.com>
2018-05-07 04:06:18 -0300

    pequena fix na busca, climbing agora tenta selecionar até 5 nós

--- main.py
+++ main.py
@@ -10,16 +10,28 @@ edges = functions.carrega_edges()
 #features = functions.carrega_features()
 
 pre = datetime.datetime.now()
-node = algorithms.climb(edges)
+selection = algorithms.climb(edges)
 pos = datetime.datetime.now()
-print("Climbing demora: "+ str(pos-pre))
-print("Qtd de nós vizitados: " + str(len(algorithms.ginfluences)))
-print(algorithms.calculateInfluence(node))
+print("Resultado da Heurística:")
+print(" Climbing demora: "+ str(pos-pre))
+print(" Qtd de nós vizitados: " + str(len(algorithms.ginfluences)))
+count = 0
+for n in selection:
+    count += 1
+    print(" ", count, "# Nó escolhido:", n)
+    print(" Valor de infuencia do nó:", algorithms.calculateInfluence(n))
 #print(features['35415466'])
 
 # Exemplo de assert usando a busca
 ## Heurítica achou que o nó '12831' é um influenciador com uma influencia de 237
-if busca.assert_influence(edges, '12831', 237):
-    print('12831 de fato influencia 237')
-else:
-    print('12831 não influencia 237')
+print("----------------------------")
+print("Verificação utilizando busca em profundidade:")
+count = 0
+for n in selection:
+    count += 1
+    result = busca.assert_influence(edges, n, algorithms.calculateInfluence(n))
+    if result[0]:
+        print(' #', count, 'Nó', n, ': valor de infuência verificado')
+    else:
+        print(' #', count, 'Nó', n, ': valor de influência não corresponde. Busca retornou:',
+              result[1])
